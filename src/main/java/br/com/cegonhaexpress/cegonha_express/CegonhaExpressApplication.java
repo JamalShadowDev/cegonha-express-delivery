4b7e7283de4f0dd81a2563785ef2e40fe9700f39
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2025-05-23 09:59:57 -0300

    chore(start.spring): Adição da estrutura básica de diretórios
    Com isso, poderemos iniciar o desenvolvimento do aplicação.

---
+++ src/main/java/br/com/cegonhaexpress/cegonha_express/CegonhaExpressApplication.java
@@ -0,0 +1,13 @@
+package br.com.cegonhaexpress.cegonha_express;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CegonhaExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CegonhaExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cegonhaexpress/cegonha_express/CegonhaExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cegonhaexpress.cegonha_express;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CegonhaExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
